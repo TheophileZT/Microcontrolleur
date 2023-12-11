a5f0ac01de02da0d0ed1ecfffcda6bdd31b32be0
S-Kelian <kelian.sbc@gmail.com>
2023-12-11 11:46:05 +0100

    A priori c est fini

--- FINAL/Source/Principal.c
+++ FINAL/Source/Principal.c
@@ -38,23 +38,8 @@ int main (void){
 	MyTimer_Base_Init(TIM4, 1799, 1);//pour un signal de la PWM de fréquence 20 kHz
 	MyTimer_Base_Start(TIM4);
 	MyTimer_PWM(TIM4, 1);//TIM4_CH1 connecté à la pin PB6 -> PLATEAU_PWM*/
-
-	//TEST
-	/*MyTimer_Base_Init(TIM2, 1799, 1);
-	MyTimer_Base_Start(TIM2);
-	MyTimer_PWM(TIM2, 1);//TIM2_CH1 connecté à la pin PA0 -> PLATEAU_PWM
-	MyTimer_PWM_SetDutyCycle(TIM2, 1, 1);
-	
-	MyGPIO_Init(GPIOC, 3, Out_Ppull);//PLATEAU_DIRECTION PA4 -> 20e pin
-	MyGPIO_Reset(GPIOC, 3);//bit de sens à 1 pour indiquer le sens de rotation
-	MyUSART_Send(USART3, ptr);
-	MyGPIO_Set(GPIOA, 4);
-	MyTimer_PWM_SetDutyCycle(TIM2, 1, 1);*/
-	
-	MyUSART_Send(USART3, ptr);
 	
 	do {
-		//MyUSART_Send(USART3,ptr);
 		bordage();
 	} while(1);
 }
