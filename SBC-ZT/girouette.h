ff424bd725eeb9f22268409587464ce6d79b2179
S-Kelian <80928195+S-Kelian@users.noreply.github.com>
2023-12-16 21:58:59 +0100

    Delete SBC-ZT directory

--- SBC-ZT/girouette.h
+++
@@ -1,7 +0,0 @@
-#ifndef GIROUETTE_H
-#define GIROUETTE_H
-
-void GirouetteInit(void);
-float GirouetteAngle(void);
-
-#endif
--- SBC-ZT/servo.h
+++
@@ -1,8 +0,0 @@
-#ifndef SERVO_H
-#define SERVO_H
-
-void servoInit(void);
-void bordage(void);
-
-
-#endif
